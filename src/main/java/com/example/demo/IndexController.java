735d7049a7a6be61c134a7ba27e3ae4ef8dd0dd8
Yuting Wang <yw4554@nyu.edu>
2024-04-14 10:00:55 -0500

    init commit

---
+++ src/main/java/com/example/demo/IndexController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
